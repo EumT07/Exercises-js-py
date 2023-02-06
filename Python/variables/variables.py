707c880c906b4392e33e5233729a2bf0d74d3103
EumT07 <eublanmata@gmail.com>
2023-02-06 13:18:05 -0400

    Creating repository on github

--- Python/variables/variables.py
+++ Python/variables/variables.py
@@ -5,24 +5,30 @@ import keyword
 #Data Types
 
 #String
-# print("Hello world")
-# print('Hello world')
-# print("""Hello world""") #Multy lines
+print("Hello world")
+print('Hello world')
+print("""Hello world""") #Multy lines
 
 #Numbers
-# print(23) #Int
-# print(3.14) #float
+print(23) #Int
+print(3.14) #float
 
 #Boolean
-# print(True) # -> 0
-# print(False) # -> 1
+print(True) # -> 0
+print(False) # -> 1
 
 #Variables
 
-name = "Miguelito"
-last_Name = "Guillermo"
-age = 30
-DNI = 4543234
+name = "Miguelito" #Normal
+last_Name = "Guillermo" #Snake_Case
+age = 30 #Normal
+numPI = 3.14 #Camelcase
+DNI = 4543234 #Normal -> Like Const in js
+
+#type
+print(type(name)) #Str : <class 'str'> 
+print(type(age)) #Int : <class 'int'> 
+print(type(numPI)) #Float : <class 'float'>
 
 #1 
 print("name: ", name)
