218f0bc793658c5ee975b7a079d1c793081dc8d3
EumT07 <eublanmata@gmail.com>
2023-02-15 16:44:19 -0400

    Search methods: Part II and Part III done

--- Javascript/5. String/search.js
+++ Javascript/5. String/search.js
@@ -1,16 +1,16 @@
-//Search Methods
+//Search and text Editing Methods
 
 /**
  * Part I: It allows us to search the index position of any words
  * 
- *          indexof(): Return index position *Start*
+ *          indexof():
  *          lasIndexOf()
- *          search() : search the string in the text
+ *          search()
  *          match()
  *          chartA()
  * 
  **/
-
+console.log("* * * Part I * * *\n");
 let text = "There is a big house, between that mountains and that green forest, they look amazing";
 
 //Index OF
@@ -39,3 +39,74 @@ console.log(`\tSearch: The word -> '${word1}' is in the position: `,result_searc
 console.log(`\tChartAt: 'bet-w-een' we insert 25 and we got w: `,result_chartAt);
 
 
+//Part II: It allows know is any string has any words, letter o numbers inside it.
+
+console.log("\n* * * Part II * * *\n");
+let message = "Today, is a beautiful day, so for that reason I'm going to the beach";
+
+console.log("startwith(): ",message.startsWith("Today"));
+console.log("endswith(): ",message.endsWith("beach"));
+
+//Include
+//@param searchString — search string
+//@param position — If position is undefined, 0 is assumed, so as to search all
+console.log("includes(): ",message.includes("is", 8));
+
+// Part III: Methods to replace, subtring, slice, split and trim text
+/**
+ * repeat()
+ * replace()
+ * slice()
+ * substring()
+ * split()
+ * trim()
+ * 
+*/
+
+console.log("\n* * * Part III * * *\n");
+
+// Repeat()
+let stringText = " Hello ";
+let result_A = stringText.repeat(4);
+console.log("repeat(): ",result_A);
+
+//replace()
+let stringText2 = "I'm learning javascript";
+let result_B = stringText2.replace("javascript", "python");
+console.log("replace(): ",result_B);
+
+//slice(start,end)
+//start: it starts counting and cuttingt the text
+//end: the last position when finishes counting
+let stringText3 = "This text is going to be a little bit short than few minutes ago";
+
+let index_1 = stringText3.indexOf("bit");
+console.log(index_1);
+let result_C = stringText3.slice(index_1, 44);
+console.log("slice(): ",result_C);
+
+//substring(start,end)
+let stringText4 = "You can flay";//can 
+let result_D = stringText4.substring(4,7);
+console.log("substring(): ",result_D);
+
+//Split: Transform string into an array
+let stringText5 = "Hello my friend today I'm going to tell you something really important";
+let result_E = stringText5.split(' ');
+console.log("split(' '): ",result_E);
+
+let stringText6 = "Hello-my-friend-today-I'm-going-to-tell-you-something-really-important";
+let result_F = stringText6.split("-");
+console.log("split('-'): ",result_F);
+
+//trim(): it deletes any space
+let message2 = "     hola     ";
+
+//trimStart(): it deletes any space at start of the text 
+console.log("trimStart():",message2.trimStart());
+
+//trim(): it deletes any space both start/end
+console.log("trim():",message2.trim());
+
+//trimEnd(): it deletes any space at the end of the text 
+console.log("trimEnd():",message2.trimEnd());
