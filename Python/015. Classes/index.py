4034a6878de9ea7efa7c23cdf6cf0e81752c78e2
EumT07 <eublanmata@gmail.com>
2023-03-30 11:06:45 -0400

    classes done

--- Python/015. Classes/index.py
+++ Python/015. Classes/index.py
@@ -31,5 +31,71 @@ class Car:
             print("This car is old")
 
 car1 = Car("mustang","white",4)
-print(car1.new)
-print(car1.precio)
+
+print(car1)
+print("-----")
+car1.usado()
+car1.precio()
+print("-----")
+
+#Methods: Init, del, str,len
+
+class Lesson:
+    #Class Constructor
+    def __init__(self,title,duration,start):
+        self.title = title
+        self.duration = duration
+        self.start = start
+        print(f"Lesson: {self.title}")
+
+    #Delete class
+    def __def__(self):
+        return f"{self.title} was deleted from our class' list"
+    
+    #string 
+    def __str__(self):
+        return f"{self.title} will start at {self.start} am and has a duration {self.duration} minutes"
+    
+    #length
+    def __len__(self):
+        return self.duration
+
+english = Lesson("Verbo to be", 45,8)
+
+print(english)
+
+#Object in object
+
+class Movie:
+    #Constructor de clase
+    def __init__(self, title, duration, release):
+        self.title = title
+        self.duration = duration
+        self.release = release
+        print(f"Film: {self.title}")
+    
+    #Redefiniendo el metodo string
+    def __str__(self):
+        return f"Movie: {self.title} Year: ({self.release}) duration {self.duration} minutes"
+  
+class Catalogue:
+    movies = [] #Atributo
+    def __init__(self,movies=[]):
+        self.movies = movies
+    def add(self,movie):
+        self.movies.append(movie)
+    def show(self):
+        for p in self.movies:
+            print(p)
+
+movie1 = Movie("La monja",202,2018)
+movie2 = Movie("El cuervo",150,1992)
+print(movie1)
+print(movie2)
+
+c = Catalogue([movie1])
+c.show()
+c.add(movie2)
+c.add(Movie("Capitan rock",45,2015))
+c.add(Movie ("Lluvia",85,2008))
+c.show()
